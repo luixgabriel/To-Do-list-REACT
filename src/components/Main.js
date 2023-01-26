cceed9186af4231601cdb5853ca0bd8270353629
luix gabriel <luisgasbriel@gmail.com>
2023-01-26 15:01:33 -0300

    styling components css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 
+import { FaPlus, FaEdit, FaWindowClose } from 'react-icons/fa';
+import './Main.css';
+
 export default class Main extends Component {
   state = {
     novaTarefa: '',
+    Tarefas: [
+      'Treinar',
+      'Estudar react',
+      'Estudar cloud',
+      'Estudar typescript',
+    ],
   };
 
   handleChange = (e) => {
@@ -12,15 +21,32 @@ export default class Main extends Component {
   };
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, Tarefas } = this.state;
+
     return (
       <div className="main">
-        <h1>{ novaTarefa }</h1>
+        <h1>Lista de Tarefas:</h1>
 
-        <form action="#">
-          <input onChange={this.handleChange} type="text" />
-          <button type="submit">Enviar</button>
+        <form action="#" className="form">
+          <input onChange={this.handleChange} type="text" value={novaTarefa} />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
+        <ul className="list">
+          {
+            Tarefas.map((tarefa) => (
+              <li key={tarefa}>
+                {tarefa}
+                <div>
+                  <FaEdit className="edit" />
+                  <FaWindowClose className="delete" />
+                </div>
+              </li>
+
+            ))
+          }
+        </ul>
       </div>
     );
   }
