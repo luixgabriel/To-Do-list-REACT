cc18bfc37fda06afe415a220902fc0bc9a5db8d5
luix gabriel <luisgasbriel@gmail.com>
2023-01-26 15:56:30 -0300

    Creating add To Do button logic

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,5 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-console */
 import React, { Component } from 'react';
 
 import { FaPlus, FaEdit, FaWindowClose } from 'react-icons/fa';
@@ -6,17 +8,27 @@ import './Main.css';
 export default class Main extends Component {
   state = {
     novaTarefa: '',
-    Tarefas: [
-      'Treinar',
-      'Estudar react',
-      'Estudar cloud',
-      'Estudar typescript',
-    ],
+    Tarefas: [],
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { Tarefas } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim();
+    if (Tarefas.indexOf(novaTarefa) !== -1) return;
+
+    const novasTarefas = [...Tarefas];
+
+    this.setState({
+      Tarefas: [...novasTarefas, novaTarefa],
+    });
   };
 
   handleChange = (e) => {
     this.setState({
       novaTarefa: e.target.value,
+
     });
   };
 
@@ -27,7 +39,7 @@ export default class Main extends Component {
       <div className="main">
         <h1>Lista de Tarefas:</h1>
 
-        <form action="#" className="form">
+        <form onSubmit={this.handleSubmit} action="#" className="form">
           <input onChange={this.handleChange} type="text" value={novaTarefa} />
           <button type="submit">
             <FaPlus />
@@ -38,10 +50,10 @@ export default class Main extends Component {
             Tarefas.map((tarefa) => (
               <li key={tarefa}>
                 {tarefa}
-                <div>
+                <span>
                   <FaEdit className="edit" />
                   <FaWindowClose className="delete" />
-                </div>
+                </span>
               </li>
 
             ))
